972319997693f73a1037c16166933239776941b0
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-01-28 00:44:03 +0200

    fix: change fetch rate logik

--- controllers/rate.js
+++ controllers/rate.js
@@ -1,15 +1,32 @@
 const getCurrentRate = require('../puppeteer');
+const http = require('https');
+const SITE = 'https://viknocenter.ua/';
+
 // const ctrl = require('../controllers/rate');
 
 // const { createError } = require('../utils/createError');
 
 const rate = async (req, res, next) => {
-  const currentRate = await getCurrentRate();
-  console.log('currentRate', currentRate);
+  let result = null;
+  const data = [];
+  const reqw = await http.request(SITE, async resp => {
+    await resp.on('data', _ => {
+      data.push(_);
+    });
+    await resp.on('end', () => {
+      const resFullString = data.join();
+      const start = data.join().indexOf('<h3');
+      const end = data.join().indexOf('</h3');
 
-  res.status(200).json({
-    currentRate,
+      result = resFullString
+        .slice(start, end)
+        .split(' ')[3];
+      res.status(200).json({
+        currentRate: result,
+      });
+    });
   });
+  reqw.end();
 };
 
 module.exports = rate;
--- puppeteer.js
+++ puppeteer.js
@@ -1,24 +1,30 @@
+const http = require('https');
 const puppeteer = require('puppeteer');
+const { start } = require('repl');
 console.log('hi');
-
-const SITE = 'https://viknocenter.ua/';
 
 const getCurrentRate = async () => {
   try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.goto(SITE);
-    const searchResultSelector = 'h3';
-    const waitForSelector = await page.waitForSelector(
-      searchResultSelector
-    );
-    const fullTitle = await waitForSelector.evaluate(
-      el => el.textContent
-    );
-    const rate = fullTitle.split(' ')[2];
-    await browser.close();
-    return rate;
+    let result = null;
+    const data = [];
+    const req = await http.request(SITE, async res => {
+      await res.on('data', _ => data.push(_));
+      await res.on('end', () => {
+        const resFullString = data.join();
+        const start = data.join().indexOf('<h3');
+        const end = data.join().indexOf('</h3');
+        console.log(
+          resFullString.slice(start, end).split(' ')[3]
+        );
+        result = resFullString
+          .slice(start, end)
+          .split(' ')[3];
+      });
+    });
+    console.log('req', req);
+    req.end();
+    console.log('result', result);
+    return result;
   } catch (error) {
     console.log(error);
   }
