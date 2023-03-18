3d75d088b6fb03c3010982c63fb65a5f68d265c9
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-03-18 19:08:43 +0200

    fix: fecth new token in ctrl

--- controllers/course.js
+++ controllers/course.js
@@ -4,13 +4,18 @@ const SITE = `http://api.wisey.app/api/v1/core/preview-courses/`;
 
 const course = async (req, res, next) => {
   console.log('hi');
-  const token = req.headers.authorization || req.headers.Authorization;
+  const data = await axios.get(
+    'http://api.wisey.app/api/v1/auth/anonymous?platform=subscriptions'
+  );
+  const token = data.data.token;
+  console.log('token', token);
+  // const token = req.headers.authorization || req.headers.Authorization;
   const params = req.params;
   const reqPath = params.currentCourse;
 
   const response = await axios.get(SITE + reqPath, {
     headers: {
-      authorization: token,
+      authorization: `Bearer ${token}`,
     },
   });
   res.status(200).json({
